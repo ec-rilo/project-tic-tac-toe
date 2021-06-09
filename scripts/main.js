348784fcbc7a0e4aca9efcedb9fcbc7abb88b7af
Edgar Carrillo <ecarrillo046@gmail.com>
2021-06-09 10:12:50 -0700

    Add game UI

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+(function () {
+    'use strict';
+
+    let tileContainer = 'undefined';
+
+    let app = {
+        initialize: function () {
+            
+            tileContainer = document.querySelector('.tile-container');
+            const _numOfTiles = 3;
+            for (let i = 0; i < _numOfTiles; ++i) {
+                let _row = document.createElement('div');
+                _row.setAttribute('class', 'row');
+
+                for (let j = 0; j < 3; ++j) {
+                    let _tile = document.createElement('div');
+                    _tile.setAttribute('class', 'tiles');
+                    _row.appendChild(_tile);
+                }
+
+                tileContainer.appendChild(_row);
+            }
+        },
+    };
+
+    app.initialize();
+})();
